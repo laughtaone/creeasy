35ebea8c133be37ded20246eaa70cfdfb942cae6
Taichi USUBA <m21018@g.metro-cit.ac.jp>
2024-09-04 01:25:20 +0900

    2024/09/04/Wed 01:25

--- lib/cart_manage/card_manage.dart
+++ lib/cart_manage/card_manage.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:creeasy/main.dart';
 import 'package:creeasy/cart_manage/add_card/add_card_main.dart';
 import 'package:creeasy/cart_manage/card_manage_card-tile.dart';
+import 'package:creeasy/cart_manage/card_manage_bank-tile.dart';
+
 
 
 
@@ -13,73 +15,111 @@ class CardManagePage extends StatefulWidget {
 class _CardManagePageState extends State<CardManagePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(
-              Icons.credit_card,
-              color: Colors.black
-            ),
-            SizedBox(width: 5),
-            Text(
-              'カード管理',
-            ),
-          ],
-        ),
-        actions: [IconButton(
-          icon: Icon(Icons.add, color: Colors.black,),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AddCardPage(),
-                fullscreenDialog: true,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.fact_check_outlined,
+                color: Colors.black
               ),
-            );
-          },
-        )],
-        backgroundColor:
-            Theme.of(context).appBarTheme.backgroundColor, // Themeから色を取得
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
-        child: ListView(
+              SizedBox(width: 5),
+              Text(
+                'カード関係管理',
+              ),
+            ],
+          ),
+          actions: [IconButton(
+            icon: Icon(Icons.add, color: Colors.black,),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AddCardPage(),
+                  fullscreenDialog: true,
+                ),
+              );
+            },
+          )],
+          backgroundColor: Theme.of(context).appBarTheme.backgroundColor, // Themeから色を取得
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.credit_card),
+                    SizedBox(width: 5),
+                    Text('カード'),
+                  ],
+                ),
+              ),
+              Tab(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.account_balance_outlined),
+                    SizedBox(width: 5),
+                    Text('銀行'),
+                  ],
+                ),
+              )
+            ]
+          ),
+        ),
+        body: TabBarView(
           children: [
-            // ==================================== 編集方法の案内 ====================================
-            // Container(
-            //   padding: EdgeInsets.all(10),
-            //   decoration: BoxDecoration(
-            //     borderRadius: BorderRadius.circular(10),
-            //     color: Color(0xffeeeeee),
-            //   ),
-            //   child: Row(
-            //     children: [
-            //       Icon(Icons.info_outline),
-            //       SizedBox(width: 10),
-            //       Text('編集するには各カードの枠内をタップ', style: TextStyle(fontWeight: FontWeight.w400),)
-            //     ],
-            //   ),
-            // ),
-            Container(
-              margin: EdgeInsets.only(top: 5, bottom: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+            // ===================================== カード タブ =========================================
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: ListView(
                 children: [
-                  Icon(Icons.info_outline),
-                  SizedBox(width: 6),
-                  Text('編集するには各カードの枠内をタップ', style: TextStyle(fontWeight: FontWeight.w400),)
-                ],
+                  Container(
+                    margin: EdgeInsets.only(top: 5, bottom: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.info_outline),
+                        SizedBox(width: 6),
+                        Text('編集するには各カードの枠内をタップ', style: TextStyle(fontWeight: FontWeight.w400),)
+                      ],
+                    ),
+                  ),
+                  CardManageCardTile(card_name: '三井住友カード', return_rate_unit: 200, return_rate: 0.5, target_range: '1日-末日', pay_date: '翌月26日', bool_pointup: true),
+                  CardManageCardTile(card_name: 'メルカード', return_rate_unit: 100, return_rate: 1, target_range: '1日-末日', pay_date: '自由', bool_pointup: false),
+                ]
               ),
             ),
             // =======================================================================================
 
-
-            CardManageCardTile(card_name: '三井住友カード', return_rate_unit: 200, return_rate: 0.5, target_range: '1日-末日', pay_date: '翌月26日', bool_pointup: true),
-            CardManageCardTile(card_name: 'メルカード', return_rate_unit: 100, return_rate: 1, target_range: '1日-末日', pay_date: '自由', bool_pointup: false),
+            // ===================================== 銀行 タブ =========================================
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 5, bottom: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.info_outline),
+                        SizedBox(width: 6),
+                        Text('編集するには各銀行の枠内をタップ', style: TextStyle(fontWeight: FontWeight.w400),)
+                      ],
+                    ),
+                  ),
+                  CardManageBankTile(bank_name: 'みんなの銀行 - 専用ボックス'),
+                  CardManageBankTile(bank_name: '三井住友銀行',),
+                  CardManageBankTile(bank_name: '三菱UFJ銀行',),
+                ],
+              ),
+            )
+            // =======================================================================================
 
           ],
         ),
---
+++ lib/cart_manage/card_manage_bank-tile.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:creeasy/cart_manage/card_manage.dart';
+import 'package:intl/intl.dart';
+
+class CardManageBankTile extends StatelessWidget {
+  const CardManageBankTile({
+    required this.bank_name,
+  });
+
+  final String bank_name; // 銀行名
+
+  // final String formatted_return_rate = formatReturnRate(return_rate);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 13),
+      child: OutlinedButton(
+        onPressed: null,
+        style: OutlinedButton.styleFrom(
+            backgroundColor: Color(0xffeeeeee),
+            side: BorderSide(color: Colors.black, width: 1),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(6),
+            ),
+            fixedSize: Size.fromHeight(80)),
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    bank_name,
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// intを受け取り¥と,をつけて返す
+String formatYen(int amount) {
+  final formatter = NumberFormat('#,###', 'ja_JP');
+  return '¥${formatter.format(amount)}';
+}
+
+// 小数部分の処理
+String formatReturnRate(double rate) {
+  // 小数点以下が0の場合は整数部分のみを表示
+  if (rate % 1 == 0) {
+    return rate.toStringAsFixed(0);
+  } else {
+    return rate.toStringAsFixed(1);
+  }
+}
